5b46c153dca825558f2ac175718a282774391342
Roope Paukku <roope.paukku@gmail.com>
2024-07-06 17:55:06 +0300

    Load and split PDF's into chunks

---
+++ data_loader.py
@@ -0,0 +1,27 @@
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.schema.document import Document
+
+DATA_PATH = "data"
+CHUNK_SIZE = 500
+CHUNK_OVERLAP = 100
+
+def load_documents(path):
+    document_loader = PyPDFDirectoryLoader(path)
+    return document_loader.load()
+
+def split_documents(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=CHUNK_SIZE,
+        chunk_overlap=CHUNK_OVERLAP,
+        length_function=len,
+        is_separator_regex=False,
+    )
+    return text_splitter.split_documents(documents)
+
+def main():
+    documents = load_documents(DATA_PATH)
+    chunks = split_documents(documents)
+
+if __name__ == "__main__":
+    main()
