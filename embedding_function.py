7879150bc4e55fd3dab4bc67b173f900d4694cb0
Roope Paukku <roope.paukku@gmail.com>
2024-12-26 15:59:42 +0200

    Added gemini and generic embeddings

--- embedding_function.py
+++ embedding_function.py
@@ -1,8 +1,15 @@
 import getpass
 import os
 from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from sentence_transformers import SentenceTransformer
 
-def get_embedding_function():
-    os.environ["OPENAI_API_KEY"] = getpass.getpass()
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+
+def get_embedding_function(model="gemini"):
+    if model == "openai":
+        embeddings = OpenAIEmbeddings(model="text-embedding-3-large", show_progress_bar=True)
+    elif model == "gemini":
+        embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", show_progress_bar=True)
+    else:
+        embeddings = SentenceTransformer('all-MiniLM-L6-v2')
     return embeddings
