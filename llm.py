de39d3e1346c94f8291150d4805f6777d1916ec1
Roope Paukku <roope.paukku@gmail.com>
2024-12-26 19:05:49 +0200

    Update to allow system prompt. Also fix Gemini.

--- llm.py
+++ llm.py
@@ -1,21 +1,35 @@
-from langchain_community.llms.ollama import Ollama
+import ollama
 import google.generativeai as gemini
 from openai import OpenAI
 import os
 
-def prompt_model(prompt, model_type, model_name):
+def prompt_model(prompt, 
+                 model_type="gemini", 
+                 model_name="gemini-2.0-flash-exp", 
+                 sys_prompt="You are a helpful travel assistant. Do not use highlighted or bolded words (words like **title**), just use plain text.") -> str:
     """
     Generic function to use different models for generating responses.
     Saves space from other files.
     """
     if model_type == "ollama":
-        model = Ollama(model=model_name)
-        response_text = model.invoke(prompt)
+        response = ollama.chat(model=model_name, messages=[
+            {
+            'role': 'system',
+            'content': sys_prompt,
+            },
+            {
+                'role': 'user',
+                'content': prompt,
+            },
+        ])
+        response_text = response['message']['content']
 
     elif model_type == "gemini":
-        gemini.configure(api_key=os.environ["GEMINI_API_KEY"])
-        model = gemini.GenerativeModel(model_name)
-        response_text = model.generate_content(prompt)
+        gemini.configure(api_key=os.environ["GOOGLE_API_KEY"])
+        model = gemini.GenerativeModel(model_name,
+                                       system_instruction=[sys_prompt])
+        response = model.generate_content(prompt)
+        response_text = response.candidates[0].content.parts[0].text
 
     elif model_type == "openai":
         client = OpenAI(api_key=os.environ["OPENAI_API_KEY"],
@@ -23,6 +37,10 @@ def prompt_model(prompt, model_type, model_name):
         completion = client.chat.completions.create(
             model=model_name,
             messages=[
+                {
+                'role': 'system',
+                'content': sys_prompt,
+                },
                 {
                     "role": "user",
                     "content": prompt
