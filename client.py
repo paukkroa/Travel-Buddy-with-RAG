b2cd38da71e978af463c58b40d6075b6ba4dfaec
Roope Paukku <roope.paukku@gmail.com>
2024-12-27 15:04:07 +0200

    Chat history feature

--- client.py
+++ client.py
@@ -1,13 +1,22 @@
 import os
 import time
+from langchain.prompts import ChatPromptTemplate
 
+# Silence warnings
+import warnings
+warnings.filterwarnings('ignore')
+os.environ["GRPC_VERBOSITY"] = "ERROR"
+os.environ["GLOG_minloglevel"] = "2"
+
+# Import functions from other files
 from create_or_update_db import create_or_update_chroma, clear_database
 from data_loader import load_documents, split_documents
-from embedding_function import get_embedding_function
-from rag import query_and_response, query_only
+from rag import query_and_response
 from llm import prompt_model
 from logger import get_logger
-from llm_utils import sys_prompts
+from llm_utils import sys_prompts, PROMPT_TEMPLATE
+import db
+import hashlib
 
 class TravelBuddyCLI():
     def __init__(self, 
@@ -18,6 +27,7 @@ class TravelBuddyCLI():
                  chunk_size = 500,
                  chunk_overlap = 100):
         self.model_type = model_type
+        self.client_name = "TravelBuddy"
         self.model_name = model_name
         self.chroma_path = chroma_path
         self.data_path = data_path
@@ -30,10 +40,10 @@ class TravelBuddyCLI():
                              "/query": "Ask a general question.",
                              "/update_db": "Update the data and database.",
                              }
-        self.session_chat_history = []
-        self.logger = get_logger("TravelBuddyCLI")
+        self.logger = get_logger(f"{self.client_name}")
         self.last_checked_path = "last_checked.txt"
         self.query_k_results = 10
+        self.conn = db.create_connection()
 
     def _get_last_modified_time(self, directory):
         """
@@ -52,7 +62,7 @@ class TravelBuddyCLI():
                                       chunk_size = self.chunk_size, 
                                       chunk_overlap = self.chunk_overlap)
 
-    def _update_db(self):
+    def _update_vector_db(self):
         """
         Update the chroma database with the new chunks.
         """
@@ -61,7 +71,7 @@ class TravelBuddyCLI():
                                 chunks = self.chunks, 
                                 model = self.model_type)
 
-    def _clear_db(self):
+    def _clear_vector_db(self):
         """
         Empty the chroma database.
         """
@@ -87,56 +97,138 @@ class TravelBuddyCLI():
         except FileNotFoundError:
             self.last_checked_time = None
 
-    def start(self):
-        self.logger.info("Starting TravelBuddy CLI")
+    def prepare_chat(self):
+        if self.conn is None:
+            self.logger.error("Could not connect to the database, chat history is not available.")
+            self.username = "You"
+        
+        # Create tables if they do not exist
+        db.create_tables(self.conn)
+
+        # Get the chat ID
+        self.chat_id = db.get_most_recent_chat_id(self.conn)
+
+        if self.chat_id is None:
+            self.username = input(f"Welcome to {self.client_name}! Before we begin, please enter your name:\nYour name: ")
+            self.chat_id = hashlib.md5(self.username.encode()).hexdigest()
+            self.chat_description = f"Chat with {self.username}"
+            self.context_type = "last_n"
+            self.context_length = 10
+            db.create_chat(self.conn, self.chat_id, self.username, self.chat_description, self.context_type, self.context_length)
+        else:
+            chat_info = db.get_chat_info(self.conn, self.chat_id)
+            self.chat_id = chat_info["chat_id"]
+            self.username = chat_info["chat_name"]
+            self.chat_description = chat_info["chat_description"]
+            self.context_type = chat_info["context_type"]
+            self.context_length = chat_info["context_length"]
+            print(f"TravelBuddy: Welcome back, {self.username}!")
+
+    def prepare_data(self):
         self._update_last_checked_time()
         # Check if the data has been modified since the last check.
         last_modified_time = self._get_last_modified_time(directory = self.data_path)
         if self.last_checked_time is None or last_modified_time > self.last_checked_time:
             self._load_data()
-            self._update_db()
+            self._update_vector_db()
             self.last_checked_time = time.time()
             self._export_last_checked_time()
         else:
             self.logger.info("Data has not been modified since last start.")
-        
-        print("TravelBuddy CLI started. Enter '/bye' to exit.\nList more commands with '/help'.")
+
+    def main_loop(self):
         user_input = ""
+
+        # Start main loop
         while user_input != "/bye":
-            user_input = input("You: ")
+            # Init variables and get input
+            response = ""
+            chat_history = db.get_chat_messages(self.conn, self.chat_id)
+            user_input = input(f"{self.username}: ")
+
+            # Chatting with the model without RAG
             if user_input not in self.cli_commands:
-                response = prompt_model(user_input, self.model_type, self.model_name)
-                self.session_chat_history.append(user_input)
-                self.session_chat_history.append(response)
-                print("TravelBuddy: " + response)
+                # Prepare prompt with chat history
+                prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+                prompt = prompt_template.format(message=user_input, chat_history=chat_history)
+                # Get response from the model
+                response = prompt_model(prompt, self.model_type, self.model_name, sys_prompts["generic"])
+                print(f"{self.client_name}: {response}")
+
+            # Use RAG to query the database in search of a destination
             elif user_input == "/destination":
-                query_text = input("TravelBuddy: What kind of destination are you looking for?\nYou: ")
-                query_and_response(query_text, self.model_type, self.model_name, sys_prompt=sys_prompts["destination"], k=self.query_k_results)
+                query_text = input(f"{self.client_name}: What kind of destination are you looking for?\n{self.username}: ")
+                response, formatted_response = query_and_response(query_text, 
+                                                                  self.model_type, 
+                                                                  self.model_name, 
+                                                                  sys_prompt=sys_prompts["destination"], 
+                                                                  k=self.query_k_results,
+                                                                  chat_history=chat_history)
+                print(f"{self.client_name}: {formatted_response}")
+
+            # Use RAG to query the database for travel tips
             elif user_input == "/travel_tips":
-                query_text = input("TravelBuddy: What travel tips do you need?\nYou: ")
-                query_and_response(query_text, self.model_type, self.model_name, sys_prompt=sys_prompts["travel_tips"], k=self.query_k_results)
+                query_text = input(f"{self.client_name}: What travel tips do you need?\n{self.username}: ")
+                response, formatted_response = query_and_response(query_text, 
+                                                                  self.model_type, 
+                                                                  self.model_name, 
+                                                                  sys_prompt=sys_prompts["travel_tips"], 
+                                                                  k=self.query_k_results,
+                                                                  chat_history=chat_history)
+                print(f"{self.client_name}: {formatted_response}")
+
+            # Use RAG to query the database for a general question
             elif user_input == "/query":
-                query_text = input("TravelBuddy: Enter your query.\nYou: ")
-                query_and_response(query_text, self.model_type, self.model_name)
+                query_text = input(f"{self.client_name}: Enter your query.\n{self.username}: ")
+                response, formatted_response = query_and_response(query_text, 
+                                                                  self.model_type, 
+                                                                  self.model_name, 
+                                                                  sys_prompt=sys_prompts["generic_rag"],
+                                                                  k=self.query_k_results, 
+                                                                  chat_history=chat_history)
+                print(f"{self.client_name}: {formatted_response}")
+            
+            # Update the data and database
             elif user_input == "/update_db":
                 self._load_data()
-                self._update_db()
+                self._update_vector_db()
                 self.last_checked_time = time.time()
                 self._export_last_checked_time()
+                response = "Database updated."
+                print(f"{self.client_name}: {response}")
+
+            # List all commands
             elif user_input == "/help":
-                print("TravelBuddy: Here are all of the commands available:\n")
+                print(f"{self.client_name}: Here are all of the commands available:\n")
                 for command, description in self.cli_commands.items():
                     print(f"{command}: {description}")
+
+            # Exit the CLI
             elif user_input == "/bye":
                 break
+
+            # Add the user input and response to the chat history
+            db.add_message_to_chat(self.conn, self.chat_id, self.username, user_input)
+            db.add_message_to_chat(self.conn, self.chat_id, self.client_name, response)
+
+    def start(self):
+        # Prepare the CLI
+        self.logger.info(f"Starting {self.client_name} CLI")
+        self.prepare_data()
+        self.prepare_chat()
         
+        # Start the main loop
+        self.main_loop()
+        
+        # End the CLI when main loop is exited
         self.end()
     
     def end(self):
         """
         Exit the CLI.
         """
-        print("Exiting TravelBuddy CLI.")
+        print(f"Exiting {self.client_name} CLI.")
+        db.close_connection(self.conn)
         return
 
         
---
+++ db.py
@@ -0,0 +1,244 @@
+import sqlite3
+
+from logger import get_logger
+
+logger = get_logger(__name__)
+
+def create_connection(db_file: str = "chat.db") -> sqlite3.Connection:
+    """
+    Create a database connection to the SQLite database specified by db_file
+    
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        logger.info(f"Connected to {db_file}")
+    except sqlite3.Error as e:
+        logger.error(e)
+    
+    return conn
+
+def close_connection(conn: sqlite3.Connection) -> None:
+    conn.close()
+
+def create_tables(conn: sqlite3.Connection) -> None:
+    # Chats
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS D_CHAT (
+        chat_id TEXT PRIMARY KEY,
+        chat_name TEXT NOT NULL,
+        chat_description TEXT,
+        iby TEXT DEFAULT 'system',
+        uby TEXT DEFAULT 'system',
+        idate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        udate TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+    ''')
+
+    # Chat settings
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS D_CHAT_SETTINGS (
+        chat_id TEXT NOT NULL,
+        context_type TEXT DEFAULT 'last_n',
+        context_length INTEGER DEFAULT 10,
+        iby TEXT DEFAULT 'system',
+        uby TEXT DEFAULT 'system',
+        idate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        udate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (chat_id) REFERENCES D_CHAT(chat_id)
+    );
+    ''')
+
+    # Chat history
+    conn.execute('''
+    CREATE TABLE IF NOT EXISTS F_CHAT_HISTORY (
+        chat_id TEXT NOT NULL,
+        sender TEXT NOT NULL,
+        message TEXT NOT NULL,
+        timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        iby TEXT DEFAULT 'system',
+        uby TEXT DEFAULT 'system',
+        idate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        udate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        FOREIGN KEY (chat_id) REFERENCES D_CHAT(chat_id)
+    );
+    ''')
+    conn.commit()
+
+def drop_tables(conn: sqlite3.Connection) -> None:
+    conn.execute('''
+    DROP TABLE IF EXISTS D_CHAT;
+    ''')
+    conn.execute('''
+    DROP TABLE IF EXISTS D_CHAT_SETTINGS;
+    ''')
+    conn.execute('''
+    DROP TABLE IF EXISTS F_CHAT_HISTORY;
+    ''')
+    conn.commit()
+
+def create_chat(conn: sqlite3.Connection, 
+                chat_id: str, 
+                chat_name: str, 
+                chat_description: str = None,
+                context_type: str = 'last_n',
+                context_length: int = 10) -> None:
+    """
+    Create a chat.
+    
+    :param conn: Connection object
+    :param chat_id: Chat ID
+    :param chat_name: Chat name
+    :param chat_description: Chat description
+    """
+    conn.execute('''
+    INSERT INTO D_CHAT (chat_id, chat_name, chat_description) VALUES (?, ?, ?)
+    ''', (chat_id, chat_name, chat_description))
+
+    conn.execute('''
+    INSERT INTO D_CHAT_SETTINGS (chat_id, context_type, context_length) VALUES (?, ?, ?)
+    ''', (chat_id, context_type, context_length))
+
+    conn.commit()
+
+def get_chat(conn: sqlite3.Connection, chat_id: str) -> tuple:
+    """
+    Get a chat.
+    
+    :param conn: Connection object
+    :param chat_id: Chat ID
+    :return: Chat info as tuple
+    """
+    cursor = conn.execute('''
+    SELECT chat_id, chat_name, chat_description FROM D_CHAT WHERE chat_id = ?
+    ''', (chat_id,))
+    
+    chat = cursor.fetchone()
+    
+    return chat
+
+
+def add_message_to_chat(conn: sqlite3.Connection, chat_id: str, sender: str, message: str) -> None:
+    """
+    Add a message to the chat history.
+    
+    :param conn: Connection object
+    :param chat_id: Chat ID
+    :param sender: Sender of the message
+    :param message: Message content
+    """
+    conn.execute('''
+    INSERT INTO F_CHAT_HISTORY (chat_id, sender, message) VALUES (?, ?, ?)
+    ''', (chat_id, sender, message))
+
+    conn.commit()
+
+def get_all_chat_messages(conn: sqlite3.Connection, chat_id: str) -> list:
+    """
+    Get all messages from a chat.
+    
+    :param conn: Connection object
+    :param chat_id: Chat ID
+    :return: List of messages
+    """
+    cursor = conn.execute('''
+    SELECT sender, message, timestamp FROM F_CHAT_HISTORY WHERE chat_id = ?
+    ''', (chat_id,))
+    
+    messages = cursor.fetchall()
+    
+    return messages
+
+def get_last_n_messages(conn: sqlite3.Connection, chat_id: str, n: int) -> list:
+    """
+    Get the last n messages from a chat.
+    
+    :param conn: Connection object
+    :param chat_id: Chat ID
+    :param n: Number of messages
+    :return: List of messages
+    """
+    cursor = conn.execute('''
+    SELECT sender, message, timestamp FROM F_CHAT_HISTORY WHERE chat_id = ? ORDER BY timestamp DESC LIMIT ?
+    ''', (chat_id, n))
+    
+    messages = cursor.fetchall()
+    
+    return messages
+
+def get_chat_settings(conn: sqlite3.Connection, chat_id: str) -> dict:
+    """
+    Get the chat settings.
+    
+    :param conn: Connection object
+    :param chat_id: Chat ID
+    :return: Dictionary of chat settings
+    """
+    cursor = conn.execute('''
+    SELECT context_type, context_length FROM D_CHAT_SETTINGS WHERE chat_id = ?
+    ''', (chat_id,))
+    
+    settings = cursor.fetchone()
+    
+    return settings
+
+def get_chat_messages(conn: sqlite3.Connection, chat_id: str) -> str:
+    """
+    Get chat messages based on the current setting
+    
+    :param conn: Connection object
+    :param chat_id: Chat ID
+    :return: String of the messages
+    """
+    settings = get_chat_settings(conn, chat_id)
+
+    context_type = settings[0]
+    context_length = settings[1]
+
+    if context_type == "last_n":
+        messages = get_last_n_messages(conn, chat_id, context_length)
+    elif context_type == "all":
+        messages = get_all_chat_messages(conn, chat_id)
+    else: 
+        messages = []    
+
+    messages_str = "\n".join([f"At {msg[2]}, {msg[0]} said: {msg[1]}" for msg in messages])
+    return messages_str
+
+def get_most_recent_chat_id(conn: sqlite3.Connection) -> str:
+    """
+    Get the chat ID with the most recent message.
+    
+    :param conn: Connection object
+    :return: Chat ID
+    """
+    cursor = conn.execute('''
+    SELECT chat_id FROM F_CHAT_HISTORY ORDER BY timestamp DESC LIMIT 1
+    ''')
+    
+    chat_id = cursor.fetchone()
+    
+    return str(chat_id[0]) if chat_id else None
+
+def get_chat_info(conn: sqlite3.Connection, chat_id: str) -> dict:
+    """
+    Get chat info.
+    
+    :param conn: Connection object
+    :param chat_id: Chat ID
+    :return: Dictionary of chat info
+    """
+    chat = get_chat(conn, chat_id)
+    settings = get_chat_settings(conn, chat_id)
+
+    chat_info = {
+        "chat_id": chat[0],
+        "chat_name": chat[1],
+        "chat_description": chat[2],
+        "context_type": settings[0],
+        "context_length": settings[1]
+    }
+
+    return chat_info
